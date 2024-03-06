162f1c1ee058d41e68588350d476d097d574762c
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-06 17:11:18 -0300

    company mapping

---
+++ ParkingLotManager.WebApi/Data/Mappings/CompanyMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ParkingLotManager.WebApi.Models;
+
+namespace ParkingLotManager.WebApi.Data.Mappings;
+
+public class CompanyMap : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {        
+        builder.ToTable("Company");
+
+        //PrimaryKey and IDENTITY_COLUMN(1, 1)
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+
+
+    }
+}
