b4af114349a166fc1e5d8280b38a1672eda50a3e
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-06 13:05:58 -0300

    ParkingSlot VO

---
+++ ParkingLotManager.WebApi/Models/ValueObjects/ParkingSlots.cs
@@ -0,0 +1,7 @@
+﻿namespace ParkingLotManager.WebApi.Models.ValueObjects;
+
+public class ParkingSlots
+{
+    public int MotorcycleParkingSlot { get; private set; }
+    public int CarParkingSlot { get; private set; }
+}
