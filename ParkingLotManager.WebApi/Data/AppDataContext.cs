715761b5f4e342e97a6775fd4350f1ef5b6a0c74
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-06 17:10:58 -0300

    pass connectionString

--- ParkingLotManager.WebApi/Data/AppDataContext.cs
+++ ParkingLotManager.WebApi/Data/AppDataContext.cs
@@ -8,6 +8,8 @@ public class AppDataContext : DbContext
     public DbSet<Company> Companies { get; set; }
     public DbSet<Vehicle> Vehicles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;Username=sa;Password=1q2w3e4r@#$");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+
+    
 }
