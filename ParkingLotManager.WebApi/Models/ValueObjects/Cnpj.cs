b54c603dec833c8ff59aeef5c2e1cdba5dc04c48
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-06 13:55:53 -0300

    Cnpj VO

---
+++ ParkingLotManager.WebApi/Models/ValueObjects/Cnpj.cs
@@ -0,0 +1,18 @@
+﻿namespace ParkingLotManager.WebApi.Models.ValueObjects;
+
+public class Cnpj
+{
+    public Cnpj(string cnpjNumber)
+    {
+        CnpjNumber = cnpjNumber;
+        
+        if(string.IsNullOrEmpty(CnpjNumber))
+            throw new Exception("Please type your CNPJ number");
+        if (!IsValid)
+            throw new Exception("Invalid CNPJ");
+    }
+
+    public string CnpjNumber { get; }
+
+    public bool IsValid => CnpjNumber.Length < 14 ? false : true;
+}
