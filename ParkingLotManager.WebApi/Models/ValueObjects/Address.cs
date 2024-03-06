417786059d114885ad38c1a45f2b56a7350ab6f5
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-06 13:05:51 -0300

    Address VO

---
+++ ParkingLotManager.WebApi/Models/ValueObjects/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingLotManager.WebApi.Models.ValueObjects;
+
+public class Address
+{
+    public Address(string street, string city, string zipCode)
+    {
+        Street = street;
+        City = city;
+        ZipCode = zipCode;
+    }
+
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string ZipCode { get; private set; }
+}
