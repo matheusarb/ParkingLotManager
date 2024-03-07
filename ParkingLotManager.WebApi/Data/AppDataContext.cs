fcde119360de1a74f17bb2049e8b445d07c49b3a
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-07 10:13:42 -0300

    add map configuration

--- ParkingLotManager.WebApi/Data/AppDataContext.cs
+++ ParkingLotManager.WebApi/Data/AppDataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ParkingLotManager.WebApi.Data.Mappings;
 using ParkingLotManager.WebApi.Models;
 
 namespace ParkingLotManager.WebApi.Data;
@@ -11,5 +12,9 @@ public class AppDataContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
 
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CompanyMap());
+        modelBuilder.ApplyConfiguration(new VehicleMap());
+    }
 }
