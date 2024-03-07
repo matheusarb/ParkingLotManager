514d6c2c1a596f9222759e9d1421d647d327d332
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-07 16:21:07 -0300

    refactor model

--- ParkingLotManager.WebApi/Models/Company.cs
+++ ParkingLotManager.WebApi/Models/Company.cs
@@ -4,13 +4,10 @@ namespace ParkingLotManager.WebApi.Models;
 
 public class Company
 {
-    public int Id { get; }
     public string Name { get; private set; }
     public Cnpj Cnpj { get; private set; }
     public Address Address { get; private set; }
     public string Telephone { get; private set; }
-    public ParkingSlots MotorcycleParkingSlots { get; private set; }
-    public ParkingSlots CarParkingSlots { get; private set; }
+    
     public IList<Vehicle> Vehicles { get; private set; }
-
 }
