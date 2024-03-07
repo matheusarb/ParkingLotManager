19eda4c7d36e382cbb4638452717ba5fe7cf4cfb
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-07 16:22:01 -0300

    add DbContextService

--- ParkingLotManager.WebApi/Program.cs
+++ ParkingLotManager.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using ParkingLotManager.WebApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDataContext>(op => op.UseSqlServer(connectionString));
+
 
 var app = builder.Build();
 
