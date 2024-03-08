bf6ca39d79c509dd924c8f1938399cedbb88077a
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-08 16:34:38 -0300

    add controller

---
+++ ParkingLotManager.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace ParkingLotManager.WebApi.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public IActionResult CheckStatus()
+    { 
+        return Ok(new
+        {
+            message = "API online"
+        });
+    }
+}
