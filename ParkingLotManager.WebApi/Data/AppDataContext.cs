f6d3e726b895ff5f36c8ac4d04ed40a5433a3dad
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-07 22:47:49 -0300

    changed OnConfiguring

--- ParkingLotManager.WebApi/Data/AppDataContext.cs
+++ ParkingLotManager.WebApi/Data/AppDataContext.cs
@@ -1,21 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ParkingLotManager.WebApi.Data.Mappings;
 using ParkingLotManager.WebApi.Models;
+using System.Diagnostics;
 
 namespace ParkingLotManager.WebApi.Data;
 
 public class AppDataContext : DbContext
 {
-    public AppDataContext(DbContextOptions<AppDataContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<Company> Companies { get; set; }
     public DbSet<Vehicle> Vehicles { get; set; }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //    => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
