cfbcb69f536011115866aec2e7f213c82d9b48fd
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-08 16:34:31 -0300

    add controller

---
+++ ParkingLotManager.WebApi/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkingLotManager.WebApi.Data;
+
+namespace ParkingLotManager.WebApi.Controllers;
+
+[ApiController]
+public class CompanyController : ControllerBase
+{
+    [HttpGet("v1/companies")]
+    public IActionResult Get([FromServices] AppDataContext ctx)
+    {
+        var companies = ctx.Companies.AsNoTracking().ToList();
+        return Ok(companies);
+    }
+}
---
+++ ParkingLotManager.WebApi/Controllers/VehicleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkingLotManager.WebApi.Data;
+
+namespace ParkingLotManager.WebApi.Controllers;
+
+[ApiController]
+public class VehicleController : ControllerBase
+{
+    [HttpGet("v1/vehicles")]
+    public async Task<IActionResult> GetAsync([FromServices] AppDataContext ctx)
+    {
+        var vehicles = await ctx.Vehicles.AsNoTracking().ToListAsync();
+        return Ok(vehicles);
+    }
+}
