17e8fe67c526a550aaa5e45433ad37b08b4776ca
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-07 22:47:19 -0300

    refactored mapping

--- ParkingLotManager.WebApi/Data/Mappings/VehicleMap.cs
+++ ParkingLotManager.WebApi/Data/Mappings/VehicleMap.cs
@@ -7,60 +7,56 @@ namespace ParkingLotManager.WebApi.Data.Mappings;
 public class VehicleMap : IEntityTypeConfiguration<Vehicle>
 {
     public void Configure(EntityTypeBuilder<Vehicle> builder)
-    {        
+    {
         builder.ToTable("Vehicle");
 
-        // PrimaryKey
         builder.HasKey(x => x.LicensePlate);
         builder.Property(x => x.LicensePlate)
             .IsRequired()
             .HasColumnName("LicensePlate")
-            .HasColumnType("VARCHAR")
+            .HasColumnType("NVARCHAR")
             .HasMaxLength(8);
-        
+
         builder.Property(x => x.Brand)
             .IsRequired()
             .HasColumnName("Brand")
-            .HasColumnType("VARCHAR")
-            .HasMaxLength(30);
-        
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
         builder.Property(x => x.Model)
             .IsRequired()
             .HasColumnName("Model")
-            .HasColumnType("VARCHAR")
+            .HasColumnType("NVARCHAR")
             .HasMaxLength(80);
-        
+
         builder.Property(x => x.Color)
             .IsRequired()
             .HasColumnName("Color")
-            .HasColumnType("VARCHAR")
-            .HasMaxLength(80);
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(40);
 
         builder.Property(x => x.Type)
-            .HasConversion<int>()
             .IsRequired()
-            .HasColumnName("Type");
+            .HasConversion<int>()
+            .HasColumnName("Type")
+            .HasColumnType("INT");
 
         builder.Property(x => x.CreatedAt)
             .IsRequired()
             .HasColumnName("CreatedAt")
             .HasColumnType("SMALLDATETIME")
             .HasDefaultValue(DateTime.Now.ToUniversalTime());
-        
+
         builder.Property(x => x.LastUpdateDate)
             .IsRequired()
             .HasColumnName("LastUpdateDate")
             .HasColumnType("SMALLDATETIME")
             .HasDefaultValue(DateTime.Now.ToUniversalTime());
 
-        //Index
-        //builder.HasIndex(x => x.LicensePlate, "IX_Vehicle_LicensePlate");
-
         // Relationships
-        //builder.HasOne(x => x.CompanyId)
-        //    .WithMany(x => x.Vehicles)
-        //    .HasForeignKey("CompanyName")
-        //    .HasConstraintName("FK_Vehicle_CompanyName")
-        //    .OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(x => x.Company)
+            .WithMany(x => x.Vehicles)
+            .HasConstraintName("FK_Vehicles_Company")
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
