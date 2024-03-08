4121f2bc66e134756324984f01597743a2ac7da2
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-08 17:30:49 -0300

    modify to GetAsync()

--- ParkingLotManager.WebApi/Controllers/CompanyController.cs
+++ ParkingLotManager.WebApi/Controllers/CompanyController.cs
@@ -8,9 +8,9 @@ namespace ParkingLotManager.WebApi.Controllers;
 public class CompanyController : ControllerBase
 {
     [HttpGet("v1/companies")]
-    public IActionResult Get([FromServices] AppDataContext ctx)
+    public async Task<IActionResult> GetAsync([FromServices] AppDataContext ctx)
     {
-        var companies = ctx.Companies.AsNoTracking().ToList();
+        var companies = await ctx.Companies.AsNoTracking().ToListAsync();
         return Ok(companies);
     }
 }
