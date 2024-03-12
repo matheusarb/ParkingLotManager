d4b79ced88fff34116dd5929bcd288e8e43da33d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-11 22:45:49 -0300

    add custom constructor

--- ParkingLotManager.WebApi/Models/Vehicle.cs
+++ ParkingLotManager.WebApi/Models/Vehicle.cs
@@ -4,6 +4,19 @@ namespace ParkingLotManager.WebApi.Models;
 
 public class Vehicle
 {
+    private Vehicle() 
+    { }
+
+    public Vehicle(string licensePlate, string brand, string model, string color, EVehicleType type, string companyName)
+    {
+        LicensePlate = licensePlate;
+        Brand = brand;
+        Model = model;
+        Color = color;
+        Type = type;
+        CompanyName = companyName;
+    }
+
     public string LicensePlate { get; private set; }
     public string Brand { get; private set; }
     public string Model { get; private set; }
@@ -12,5 +25,6 @@ public class Vehicle
     public DateTime CreatedAt { get; private set; }
     public DateTime LastUpdateDate { get; private set; }
 
-    public Company Company { get; private set; }
+    public string CompanyName { get; private set; }
+    public Company? Company { get; private set; }
 }
