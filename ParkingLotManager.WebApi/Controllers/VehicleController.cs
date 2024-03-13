060bc2ada2258ba75fc8192bdcef73a15f7b1cad
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-13 16:00:23 -0300

    fix update, register and delete methods

--- ParkingLotManager.WebApi/Controllers/VehicleController.cs
+++ ParkingLotManager.WebApi/Controllers/VehicleController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ParkingLotManager.WebApi.Data;
+using ParkingLotManager.WebApi.Extensions;
 using ParkingLotManager.WebApi.Models;
+using ParkingLotManager.WebApi.ViewModels;
 
 namespace ParkingLotManager.WebApi.Controllers;
 
@@ -20,31 +22,33 @@ public class VehicleController : ControllerBase
             var vehicles = await ctx.Vehicles.AsNoTracking().ToListAsync();
             if (vehicles == null)
                 return BadRequest(new { message = "01EX1000 - Request could not be processed. Please try another time" });
-            return Ok(vehicles);
+
+            //return Ok(new ResultViewModel<List<Vehicle>>(vehicles));
+            return new JsonResult(vehicles);
         }
         catch
         {
-            return StatusCode(500, new { message = "01EX1001 falha interna no servidor" });
+            return StatusCode(500, new ResultViewModel<List<Vehicle>>("01EX1001 falha interna no servidor"));
         }
     }
 
-    [HttpGet("v1/vehicles/{licentePlate:string}")]
+    [HttpGet("v1/vehicles/{licensePlate}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GetByLicensePlateAsync([FromServices] AppDataContext ctx, [FromRoute] string licentePlate)
+    public async Task<IActionResult> GetByLicensePlateAsync([FromServices] AppDataContext ctx, [FromRoute] string licensePlate)
     {
         try
         {
-            var vehicle = await ctx.Vehicles.FirstOrDefaultAsync(x => x.LicensePlate == licentePlate);
+            var vehicle = await ctx.Vehicles.FirstOrDefaultAsync(x => x.LicensePlate == licensePlate);
             if (vehicle is null)
                 return NotFound(new { message = "01EX1002 - License plate not found." });
 
-            return Ok(vehicle);
+            return Ok(new ResultViewModel<Vehicle>(vehicle));
         }
         catch
         {
-            return StatusCode(500, new { message = "01EX1003 - Internal server error" });
+            return StatusCode(500, new ResultViewModel<Vehicle> ("01EX1003 - Internal server error"));
         }
     }
 
@@ -54,71 +58,71 @@ public class VehicleController : ControllerBase
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> RegisterVehicleAsync(
         [FromServices] AppDataContext ctx,
-        [FromBody] Vehicle model)
+        [FromBody] RegisterVehicleViewModel viewModel)
     {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<RegisterVehicleViewModel>(ModelState.GetErrors()));
         try
         {
-            await ctx.Vehicles.AddAsync(model);
+            var newVehicle = new Vehicle();
+            newVehicle.Create(viewModel, newVehicle);
+            await ctx.Vehicles.AddAsync(newVehicle);
             await ctx.SaveChangesAsync();
-            return Created($"v1/vehicles/{model.LicensePlate}", model);
+
+            return Created($"vehicles/v1/{newVehicle.LicensePlate}", new ResultViewModel<Vehicle>(newVehicle));
         }
         catch (DbUpdateException ex)
         {
-            return StatusCode(500, new { message = "01EX2000 - Could not register vehicle" });
+            return StatusCode(500, new ResultViewModel<Vehicle>("01EX2000 - Could not register vehicle"));
         }
         catch (Exception ex)
         {
-            return StatusCode(500, new { message = "01EX2001 - Internal server error" });
+            return StatusCode(500, new ResultViewModel<Vehicle>("01EX2001 - Internal server error"));
         }
 
     }
 
-    [HttpPut("v1/vehicles/{licensePlate:string}")]
+    [HttpPut("v1/vehicles/{licensePlate}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> UpdateVehicle(
-            [FromServices] AppDataContext ctx,
-            [FromRoute] string licensePlate,
-            [FromBody] Vehicle model
-        )
+    public async Task<IActionResult> UpdateVehicle([FromServices] AppDataContext ctx, [FromRoute] string licensePlate, [FromBody] UpdateVehicleViewModel viewModel)
     {
+        if (viewModel.CheckIfAllNull(viewModel))
+            return BadRequest(new ResultViewModel<UpdateVehicleViewModel>("Cannot update infos if all values"));
+
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Vehicle>(ModelState.GetErrors()));
         try
         {
             var vehicle = await ctx.Vehicles.FirstOrDefaultAsync(x => x.LicensePlate == licensePlate);
             if (vehicle == null)
-                return NotFound(new { message = "01EX3000 - Vehicle not found." });
+                return NotFound(new ResultViewModel<Vehicle>("01EX3000 - Vehicle not found."));
 
-            //vehicle.CompanyName = model.CompanyName ?? vehicle.CompanyName;
-
-
-
-
-
-
-            return Ok(model);
+            vehicle.Update(viewModel, vehicle);
+            ctx.Update(vehicle);
+            await ctx.SaveChangesAsync();
+            return Ok(new ResultViewModel<Vehicle>(vehicle));
         }
         catch
         {
-            return StatusCode(500, new { message = "01EX3001 - Internal server error" });
+            return StatusCode(500, new ResultViewModel<Vehicle>("01EX3001 - Internal server error"));
         }
     }
 
-    public async Task<IActionResult> DeleteVehicle(
-            [FromServices] AppDataContext ctx,
-            [FromRoute] string licensePlate
-        )
-    {
+    [HttpDelete("v1/vehicles/{licensePlate}")]
+    public async Task<IActionResult> DeleteVehicle([FromServices] AppDataContext ctx,[FromRoute] string licensePlate)
+    {        
         try
         {
             var vehicle = await ctx.Vehicles.FirstOrDefaultAsync(x => x.LicensePlate == licensePlate);
             if (vehicle == null)
-                return NotFound(new { message = "01EX4000 - Vehicle not found." });
+                return NotFound(new ResultViewModel<Vehicle>( "01EX4000 - Vehicle not found." ));
 
             ctx.Vehicles.Remove(vehicle);
             await ctx.SaveChangesAsync();
 
-            return Ok(vehicle);
+            return Ok(new ResultViewModel<Vehicle>(vehicle));
         }
         catch (Exception)
         {
