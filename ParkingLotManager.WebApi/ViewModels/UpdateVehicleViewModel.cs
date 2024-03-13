9dab74a9d151bef1333fd90a0ab6a1383522f2a8
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-13 16:01:48 -0300

    add UpdateVehicleViewModel

---
+++ ParkingLotManager.WebApi/ViewModels/UpdateVehicleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ParkingLotManager.WebApi.Enums;
+using ParkingLotManager.WebApi.Models;
+using System.ComponentModel;
+
+namespace ParkingLotManager.WebApi.ViewModels;
+
+public class UpdateVehicleViewModel
+{   
+    public string? LicensePlate { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public string? Color { get; set; }
+    public EVehicleType Type { get; set; }
+    public string? CompanyName { get; set; }
+
+    public bool CheckIfAllNull(UpdateVehicleViewModel viewModel)
+    {
+        Type type = viewModel.GetType();
+        var props = type.GetProperties();
+        var count = 0;
+
+        foreach (var prop in props)
+        {
+            var value = prop.GetValue(viewModel);
+            if (value == null || (prop.PropertyType.IsValueType && value.Equals(Activator.CreateInstance(prop.PropertyType))))
+                count++;
+        }
+
+        return count == props.Length;
+    }
+}
