9c2ea59593fa1040b1cfb5732bd22ddf7ac854e6
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-13 16:01:03 -0300

    add Create and Update methods inside the class

--- ParkingLotManager.WebApi/Models/Vehicle.cs
+++ ParkingLotManager.WebApi/Models/Vehicle.cs
@@ -1,22 +1,11 @@
-﻿using ParkingLotManager.WebApi.Enums;
+﻿using Microsoft.IdentityModel.Tokens;
+using ParkingLotManager.WebApi.Enums;
+using ParkingLotManager.WebApi.ViewModels;
 
 namespace ParkingLotManager.WebApi.Models;
 
 public class Vehicle
 {
-    private Vehicle() 
-    { }
-
-    public Vehicle(string licensePlate, string brand, string model, string color, EVehicleType type, string companyName)
-    {
-        LicensePlate = licensePlate;
-        Brand = brand;
-        Model = model;
-        Color = color;
-        Type = type;
-        CompanyName = companyName;
-    }
-
     public string LicensePlate { get; private set; }
     public string Brand { get; private set; }
     public string Model { get; private set; }
@@ -25,6 +14,33 @@ public class Vehicle
     public DateTime CreatedAt { get; private set; }
     public DateTime LastUpdateDate { get; private set; }
 
-    public string CompanyName { get; private set; }
     public Company? Company { get; private set; }
+    public string CompanyName { get; private set; }
+
+    public void Create(RegisterVehicleViewModel viewModel, Vehicle vehicle)
+    {
+        vehicle.LicensePlate = viewModel.LicensePlate;
+        vehicle.Model = viewModel.Model;
+        vehicle.Brand = viewModel.Brand;
+        vehicle.Color = viewModel.Color;
+        vehicle.Type = viewModel.Type;
+        vehicle.CompanyName = viewModel.CompanyName;
+    }
+    
+    public void Update(UpdateVehicleViewModel viewModel, Vehicle vehicle)
+    {
+        vehicle.LicensePlate = viewModel.LicensePlate.IsNullOrEmpty() ? vehicle.LicensePlate : viewModel.LicensePlate;
+        vehicle.Brand = viewModel.Brand.IsNullOrEmpty() ? vehicle.Brand : viewModel.Brand;
+        vehicle.Model = viewModel.Model.IsNullOrEmpty() ? vehicle.Model : viewModel.Model;
+        vehicle.Color = viewModel.Color.IsNullOrEmpty() ? vehicle.Color : viewModel.Color;
+        vehicle.Type = vehicle.Type != viewModel.Type ? viewModel.Type : vehicle.Type;
+        vehicle.CompanyName = viewModel.CompanyName.IsNullOrEmpty() ? vehicle.CompanyName : viewModel.CompanyName;
+    }
+
+    static void ChangeLicensePlate(string licensePlate, Vehicle vehicle)
+    {
+        if (licensePlate == null)
+            return;
+        vehicle.LicensePlate = licensePlate;
+    }
 }
