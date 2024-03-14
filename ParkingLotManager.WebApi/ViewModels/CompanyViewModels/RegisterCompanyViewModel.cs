0b7323873b6018e093cfd6e67731768e36d82415
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-13 21:53:03 -0300

    add 2 viewModels

---
+++ ParkingLotManager.WebApi/ViewModels/CompanyViewModels/RegisterCompanyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ParkingLotManager.WebApi.Models;
+using ParkingLotManager.WebApi.Models.ValueObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingLotManager.WebApi.ViewModels.CompanyViewModels;
+
+public class RegisterCompanyViewModel : Company
+{
+    [Required(ErrorMessage = "Company name is required")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Company CNPJ is required")]
+    public Cnpj CNPJ { get; set; }
+
+    [Required(ErrorMessage = "Company address is required")]
+    public Address Address { get; set; }
+
+    [Required(ErrorMessage = "Company telephone is required")]
+    public string Telephone { get; set; }
+
+    [Required(ErrorMessage = "Company car slots is required")]
+    public int CarSlots { get; set; }
+
+    [Required(ErrorMessage = "Company motorcycle slots is required")]
+    public int MotorcycleSlots { get; set; }
+}
+
