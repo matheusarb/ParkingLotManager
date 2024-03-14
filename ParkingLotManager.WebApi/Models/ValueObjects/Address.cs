49f9243536e6d60c097d50b8454708423681ebef
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-13 23:14:02 -0300

    add validation

--- ParkingLotManager.WebApi/Models/ValueObjects/Address.cs
+++ ParkingLotManager.WebApi/Models/ValueObjects/Address.cs
@@ -1,7 +1,25 @@
-﻿namespace ParkingLotManager.WebApi.Models.ValueObjects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingLotManager.WebApi.Models.ValueObjects;
 
 public class Address : ValueObject
 {
+    public Address(string street, string city, string zipCode)
+    {
+        if (string.IsNullOrEmpty(street))
+            throw new Exception("Type Street name");
+        
+        if (string.IsNullOrEmpty(city))
+            throw new Exception("Type City name");
+        
+        if (string.IsNullOrEmpty(zipCode))
+            throw new Exception("Type Zip Code");
+
+        Street = street;
+        City = city;
+        ZipCode = zipCode;
+    }
+        
     public string Street { get; private set; }
     public string City { get; private set; }
     public string ZipCode { get; private set; }
