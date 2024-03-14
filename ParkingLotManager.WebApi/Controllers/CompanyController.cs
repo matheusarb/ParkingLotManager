03c3a5ddc3936c202a0565c576d82ff45e40054d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-13 23:13:31 -0300

    add endpoints

--- ParkingLotManager.WebApi/Controllers/CompanyController.cs
+++ ParkingLotManager.WebApi/Controllers/CompanyController.cs
@@ -6,6 +6,7 @@ using ParkingLotManager.WebApi.Models;
 using ParkingLotManager.WebApi.ViewModels;
 using ParkingLotManager.WebApi.ViewModels.CompanyViewModels;
 using ParkingLotManager.WebApi.ViewModels.VehicleViewModels;
+using System.Data.Common;
 
 namespace ParkingLotManager.WebApi.Controllers;
 
@@ -43,7 +44,7 @@ public class CompanyController : ControllerBase
         {
             var company = await _ctx.Companies.FirstOrDefaultAsync(x => x.Name == name);
             if (company == null)
-                return NotFound(new { message = "05EX5002 - Company not foun." });
+                return NotFound(new { message = "05EX5002 - Company not found." });
 
             return Ok(new ResultViewModel<Company>(company));
         }
@@ -56,15 +57,47 @@ public class CompanyController : ControllerBase
     [HttpPost("v1/companies")]
     public async Task<IActionResult> RegisterCompanyAsync([FromBody] RegisterCompanyViewModel viewModel)
     {
-        if(!ModelState.IsValid)
+        if (!ModelState.IsValid)
             return BadRequest(new ResultViewModel<RegisterCompanyViewModel>(ModelState.GetErrors()));
         try
         {
-            return null;
+            var company = new Company();
+            company.Create(viewModel);
+            await _ctx.Companies.AddAsync(company);
+            await _ctx.SaveChangesAsync();
+
+            return Created($"v1/companies/{company.Name}", new ResultViewModel<Company>(company));
+        }
+        catch (DbException)
+        {
+            return StatusCode(500, new ResultViewModel<List<Company>>("05EX5002 - Could not register company"));
+
         }
         catch (Exception)
         {
-            return StatusCode(500, new ResultViewModel<List<Company>>("05EX5001 - Internal server error"));            
+            return StatusCode(500, new ResultViewModel<List<Company>>("05EX5003 - Internal server error"));
+        }
+    }
+
+    [HttpPut("v1/companies")]
+
+    [HttpDelete("v1/companies/{name}")]
+    public async Task<IActionResult> Delete([FromRoute] string name)
+    {
+        try
+        {
+            var company = await _ctx.Companies.FirstOrDefaultAsync(x => x.Name == name);
+            if(company == null)
+                return BadRequest(new ResultViewModel<Company>("05EX5005 - Company not found"));
+            
+            _ctx.Remove(company);
+            await _ctx.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<Company>(company));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<List<Company>>("05EX5006 - Internal server error"));
         }
     }
 }
