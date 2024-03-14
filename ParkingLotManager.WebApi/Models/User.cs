eb772af3d3932adc137505e14bd4655cc12cfe1c
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-14 15:32:15 -0300

    create UserModel

---
+++ ParkingLotManager.WebApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ParkingLotManager.WebApi.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Slug { get; set; }
+
+    public Company? Company { get; private set; }
+    public string CompanyName { get; private set; }
+}
