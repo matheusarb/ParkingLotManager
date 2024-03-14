6b8f5d4581fae8d8a3ac6a44018eebdbdddce831
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-14 12:20:20 -0300

    add TokenService

---
+++ ParkingLotManager.WebApi/Services/TokenService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ParkingLotManager.WebApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ParkingLotManager.WebApi.Services;
+
+public class TokenService
+{
+    public string GenerateToken(Company company)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
