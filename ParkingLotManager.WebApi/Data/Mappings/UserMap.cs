fdd040b45cbf70d5ebf63ea3c0ea0658108016bf
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-14 15:32:21 -0300

    map UserModel

---
+++ ParkingLotManager.WebApi/Data/Mappings/UserMap.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ParkingLotManager.WebApi.Models;
+
+namespace ParkingLotManager.WebApi.Data.Mappings;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        // Table
+        builder.ToTable("User");
+
+        // PK
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasColumnName("Email")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+
+        builder.Property(x => x.PasswordHash)
+            .IsRequired()
+            .HasColumnName("PasswordHash");
+
+        builder.Property(x => x.Slug)
+            .IsRequired()
+            .HasColumnName("Slug")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+        // Relationships
+        builder.HasOne(x => x.Company)
+            .WithMany(x => x.Users)
+            .HasForeignKey(x => x.CompanyName)
+            .HasConstraintName("FK_Vehicles_Company")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
