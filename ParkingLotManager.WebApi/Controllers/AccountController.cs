ebbda4a2642839f63d3ade445bc5dd48c811b0e6
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-14 15:32:06 -0300

    add AccountController

---
+++ ParkingLotManager.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ParkingLotManager.WebApi.Models;
+using ParkingLotManager.WebApi.Services;
+
+namespace ParkingLotManager.WebApi.Controllers;
+
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly TokenService _tokenService;
+    public AccountController(TokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("v1/login")]
+    public IActionResult Login()
+    {
+        var token = _tokenService.GenerateToken(null);
+
+        return Ok(token);
+    }
+
+    [Authorize(Roles = "admin")]
+    [Authorize(Roles = "user")]
+    [HttpGet("v1/user")]
+    public IActionResult GetUser() => Ok(User.Identity.Name);
+    
+    [Authorize(Roles = "admin")]
+    [HttpGet("v1/admin")]
+    public IActionResult GetAdmin() => Ok(User.Identity.Name);
+}
