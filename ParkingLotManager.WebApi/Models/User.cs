4d5213fd62bd182415b365ffa36275c5b8113acc
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-15 15:07:21 -0300

    add methods and IList<Role>

--- ParkingLotManager.WebApi/Models/User.cs
+++ ParkingLotManager.WebApi/Models/User.cs
@@ -1,13 +1,35 @@
-﻿namespace ParkingLotManager.WebApi.Models;
+﻿using ParkingLotManager.WebApi.ViewModels.UserViewModels;
+using SecureIdentity.Password;
+
+namespace ParkingLotManager.WebApi.Models;
 
 public class User
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PasswordHash { get; set; }
-    public string Slug { get; set; }
+    public int Id { get; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }    
+    public string Slug { get; private set; }
 
     public Company? Company { get; private set; }
     public string CompanyName { get; private set; }
+
+    public IList<Role> Roles { get; set; }
+
+    public void Create(CreateUserViewModel viewModel, string password)
+    {
+        Name = viewModel.Name;
+        Email = viewModel.Email;
+        PasswordHash = PasswordHasher.Hash(password);
+
+        Slug = viewModel.Email.Replace("@", "-").Replace(".", "-");
+        CompanyName = viewModel.CompanyName;
+    }
+
+    public void Update(UpdateUserViewModel viewModel, User user)
+    {
+        throw new NotImplementedException();
+    }
+
+
 }
