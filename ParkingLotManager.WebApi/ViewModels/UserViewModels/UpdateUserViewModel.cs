5f9156f495a526575233da64562295bf29ad52f5
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-16 19:00:53 -0300

    add props

--- ParkingLotManager.WebApi/ViewModels/UserViewModels/UpdateUserViewModel.cs
+++ ParkingLotManager.WebApi/ViewModels/UserViewModels/UpdateUserViewModel.cs
@@ -1,6 +1,8 @@
-﻿namespace ParkingLotManager.WebApi.ViewModels.UserViewModels
+﻿namespace ParkingLotManager.WebApi.ViewModels.UserViewModels;
+
+public class UpdateUserViewModel
 {
-    public class UpdateUserViewModel
-    {
-    }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }
 }
