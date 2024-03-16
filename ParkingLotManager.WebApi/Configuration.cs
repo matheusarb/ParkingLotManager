8a0bd9cce2bf54753e596e8be684e532fdfc3478
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-16 15:45:00 -0300

    add Configuration

--- ParkingLotManager.WebApi/Configuration.cs
+++ ParkingLotManager.WebApi/Configuration.cs
@@ -2,8 +2,17 @@
 
 public static class Configuration
 {
-    public static readonly string JwtKey = "IxN9fUjnX0OcZfUl3W44ew==!!!!====";
+    public static string JwtKey = "IxN9fUjnX0OcZfUl3W44ew==!!!!====";
 
-    public static readonly string ApiKeyName = "api_key";    
-    public static readonly string ApiKey = "parking_oPt4oylWx0X4wfnj";
+    public static string ApiKeyName = "api_key";    
+    public static string ApiKey = "parking_oPt4oylWx0X4wfnj";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
 }
