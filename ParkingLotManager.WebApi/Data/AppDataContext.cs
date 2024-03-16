9e211b0ba9c8f20d3694b7d04e08eb7470dffe85
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-16 15:43:41 -0300

    configure Database moved to program.cs

--- ParkingLotManager.WebApi/Data/AppDataContext.cs
+++ ParkingLotManager.WebApi/Data/AppDataContext.cs
@@ -7,12 +7,17 @@ namespace ParkingLotManager.WebApi.Data;
 
 public class AppDataContext : DbContext
 {
+    public AppDataContext(DbContextOptions options)
+        : base(options)
+    {        
+    }
+
     public DbSet<Company> Companies { get; set; }
     public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+    //protected override void OnConfiguring(DbContextOptionsBuilder options)
+    //    => options.UseSqlServer("Server=localhost,1433;Database=ParkingLotManager;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
