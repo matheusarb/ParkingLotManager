b0dc3ff0ac85f765b9902f5df7d1e1a028aaad05
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-16 15:44:48 -0300

    configure database, Load ApiKey from Configuration

--- ParkingLotManager.WebApi/Program.cs
+++ ParkingLotManager.WebApi/Program.cs
@@ -11,25 +11,14 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
 
-LoadServices(builder);
+ConfigureAuthentication(builder);
+ConfigureServices(builder);
+ConfigureMvc(builder);
 
 var app = builder.Build();
+
+LoadConfiguration(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,16 +34,47 @@ app.MapControllers();
 
 app.Run();
 
-static void LoadServices(WebApplicationBuilder builder)
+
+static void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+static void ConfigureServices(WebApplicationBuilder builder)
+{    
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<AppDataContext>(options => options.UseSqlServer(connectionString));
+    
+    builder.Services.AddTransient<TokenService>();
+}
+
+static void ConfigureMvc(WebApplicationBuilder builder)
 {
     builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
     });
+}
 
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-    builder.Services.AddDbContext<AppDataContext>();
-    builder.Services.AddTransient<TokenService>();
+static void LoadConfiguration(WebApplication app)
+{
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
 }
