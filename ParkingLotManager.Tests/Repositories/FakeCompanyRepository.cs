36157fc7a4765401f1a7a5fbda3210c559560ff5
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-17 20:41:33 -0300

    add FakeRepos

---
+++ ParkingLotManager.Tests/Repositories/FakeCompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using ParkingLotManager.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManager.Tests.Repositories;
+
+public class FakeCompanyRepository
+{
+    public Company Get(string name)
+    {
+        if(name.Equals("companytest", StringComparison.OrdinalIgnoreCase))
+            return new Company();
+        return null;
+    }
+}
--- ParkingLotManager.Tests/Repositories/FakeVehicleRepository.cs
+++ ParkingLotManager.Tests/Repositories/FakeVehicleRepository.cs
@@ -10,10 +10,26 @@ namespace ParkingLotManager.Tests.Repositories;
 
 public class FakeVehicleRepository
 {
+    public bool Get(string licensePlate)
+    {
+        if (licensePlate.Length != 7)
+            return false;
+        if(licensePlate == "000AAAA")
+            return true;
+        return false;
+    }
+    
+    public List<Vehicle> GetAll(string route)
+    {
+        var list = new List<Vehicle>();
+        if(route == "v1/vehicles")
+            return list;
+        return null;
+    }
+
     public bool GetFord(string brand)
     {
         if (brand.Equals("Ford", StringComparison.OrdinalIgnoreCase))
             return true;
         return false;
     }
-}
