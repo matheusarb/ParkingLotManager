37616af4155cacb2adf69783c02936c00402d377
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-17 18:43:48 -0300

    add FakeVehicleRepository

---
+++ ParkingLotManager.Tests/Repositories/FakeVehicleRepository.cs
@@ -0,0 +1,19 @@
+﻿using ParkingLotManager.WebApi.Models;
+using ParkingLotManager.WebApi.ViewModels.VehicleViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManager.Tests.Repositories;
+
+public class FakeVehicleRepository
+{
+    public bool GetFord(string brand)
+    {
+        if (brand.Equals("Ford", StringComparison.OrdinalIgnoreCase))
+            return true;
+        return false;
+    }
+}
