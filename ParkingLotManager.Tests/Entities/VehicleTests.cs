59830b7d87e3bd114720aa43b3e138987db46f11
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-18 10:24:31 -0300

    add entity test

--- ParkingLotManager.Tests/Entities/VehicleTests.cs
+++ ParkingLotManager.Tests/Entities/VehicleTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ParkingLotManager.WebApi.Enums;
+using ParkingLotManager.WebApi.Models;
+using ParkingLotManager.WebApi.ViewModels.VehicleViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +13,27 @@ namespace ParkingLotManager.Tests.Entities;
 [TestCategory("Vehicle")]
 public class VehicleTests
 {
+    private Vehicle _vehicle = new();
+    private readonly RegisterVehicleViewModel _registerVehicleViewModel = new RegisterVehicleViewModel("AAA0000", "Ferrari", "f-50", "red", EVehicleType.Car, "WellPark Inc");
+    private readonly UpdateVehicleViewModel _updateVehicleViewModel = new UpdateVehicleViewModel("AAA0000", "Ferrari", "f-50", "red", EVehicleType.Motorcycle, "WellPark Inc");
 
+    [TestMethod]
+    public void Should_create_vehicle_if_registerModel_is_valid()
+    {
+        _vehicle.Create(_registerVehicleViewModel);
+        Assert.IsTrue(!string.IsNullOrEmpty(_vehicle.Brand));
+    }
+
+    [TestMethod]
+    public void Should_generate_number_1_if_vehicleType_is_car()
+    {
+        Assert.AreEqual(1, (int)_registerVehicleViewModel.Type);
+    }
+    
+    [TestMethod]
+    public void Should_generate_number_2_if_vehicleType_is_motorcycle()
+    {
+        _vehicle.Update(_updateVehicleViewModel);
+        Assert.AreEqual(2, (int)_registerVehicleViewModel.Type);
+    }
 }
