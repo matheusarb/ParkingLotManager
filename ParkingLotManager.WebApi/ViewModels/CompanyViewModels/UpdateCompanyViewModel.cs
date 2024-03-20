124361f7a1a6abbbdd5796d519e37598476d5a8b
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-20 13:23:57 -0300

    add constructor

--- ParkingLotManager.WebApi/ViewModels/CompanyViewModels/UpdateCompanyViewModel.cs
+++ ParkingLotManager.WebApi/ViewModels/CompanyViewModels/UpdateCompanyViewModel.cs
@@ -6,6 +6,20 @@ namespace ParkingLotManager.WebApi.ViewModels.CompanyViewModels;
 
 public class UpdateCompanyViewModel
 {
+    public UpdateCompanyViewModel()
+    {        
+    }
+
+    public UpdateCompanyViewModel(string? name, Cnpj? cnpj, Address? address, string? telephone, int carSlots, int motorcycleSlots)
+    {
+        Name = name;
+        Cnpj = cnpj;
+        Address = address;
+        Telephone = telephone;
+        CarSlots = carSlots == 0 ? CarSlots : carSlots;
+        MotorcycleSlots = motorcycleSlots == 0 ? MotorcycleSlots : motorcycleSlots;
+    }
+
     public string? Name { get; set; }
     public Cnpj? Cnpj { get; set; }
     public Address? Address { get; set; }
