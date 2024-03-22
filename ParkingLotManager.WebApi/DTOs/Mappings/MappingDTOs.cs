82d8e7219f7fbd873d4cdabd158fee4aa86d97da
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-22 17:40:03 -0300

    adding and mapping DTOs

---
+++ ParkingLotManager.WebApi/DTOs/Mappings/MappingDTOs.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ParkingLotManager.WebApi.Models;
+
+namespace ParkingLotManager.WebApi.DTOs.Mappings;
+
+public class MappingDTOs : Profile
+{
+    public MappingDTOs()
+    {
+        CreateMap<Vehicle, VehicleDTO>().ReverseMap();
+        CreateMap<Company, CompanyDTO>().ReverseMap();
+    }
+}
---
+++ ParkingLotManager.WebApi/DTOs/VehicleDTO.cs
@@ -0,0 +1,34 @@
+﻿using ParkingLotManager.WebApi.Enums;
+using ParkingLotManager.WebApi.Models;
+
+namespace ParkingLotManager.WebApi.DTOs;
+
+public class VehicleDTO
+{
+    public string LicensePlate { get; private set; }
+    public string Brand { get; private set; }
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public EVehicleType Type { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime LastUpdateDate { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public Company? Company { get; private set; }
+    public string CompanyName { get; private set; }
+
+    public virtual object Display()
+    {
+        var vehicleDto = new
+        {
+            licensePlate = this.LicensePlate,
+            brand = this.Brand,
+            model = this.Model,
+            color = this.Color,
+            Type = this.Type,
+            CompanyName = this.CompanyName
+        };
+        
+        return vehicleDto;
+    }
+}
