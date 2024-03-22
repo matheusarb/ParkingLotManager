ca145ff8b14820b9b18d61d1d368fde7a98ea35f
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-22 17:37:17 -0300

    implementing DTOs and endpoints

--- ParkingLotManager.WebApi/Controllers/VehicleController.cs
+++ ParkingLotManager.WebApi/Controllers/VehicleController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 using ParkingLotManager.WebApi.Attributes;
 using ParkingLotManager.WebApi.Data;
+using ParkingLotManager.WebApi.DTOs;
 using ParkingLotManager.WebApi.Extensions;
 using ParkingLotManager.WebApi.Models;
 using ParkingLotManager.WebApi.ViewModels;
@@ -17,14 +19,18 @@ namespace ParkingLotManager.WebApi.Controllers;
 public class VehicleController : ControllerBase
 {
     private readonly AppDataContext _ctx;
+    private readonly IMapper _mapper;
     private const string apiKeyName = Configuration.ApiKeyName;
 
     protected VehicleController()
-    {        
+    {
     }
 
-    public VehicleController(AppDataContext ctx)
-        => _ctx = ctx;
+    public VehicleController(AppDataContext ctx, IMapper mapper)
+    {
+        _mapper = mapper;
+        _ctx = ctx;
+    }
 
     /// <summary>
     /// Get collection of vehicles
@@ -48,7 +54,9 @@ public class VehicleController : ControllerBase
             if (vehicles == null)
                 return BadRequest(new { message = "01EX1000 - Request could not be processed. Please try another time" });
 
-            return Ok(new ResultViewModel<List<Vehicle>>(vehicles));
+            var resultList = vehicles.VehiclesToDtoList(vehicles, _mapper);
+
+            return new JsonResult(resultList);
         }
         catch
         {
@@ -80,7 +88,10 @@ public class VehicleController : ControllerBase
             if (vehicle is null)
                 return NotFound(new { message = "01EX1002 - License plate not found." });
 
-            return Ok(new ResultViewModel<Vehicle>(vehicle));
+            var vehicleMapping = _mapper.Map<VehicleDTO>(vehicle);
+            var vehicleDto = vehicleMapping.Display();
+
+            return Ok(new ResultViewModel<dynamic>(vehicleDto));
         }
         catch
         {
@@ -110,7 +121,9 @@ public class VehicleController : ControllerBase
             if (fordVehicles == null)
                 return NotFound(new ResultViewModel<string>("01EX1004 - Request could not be processed. Please try again another time"));
 
-            return Ok(new ResultViewModel<List<Vehicle>>(fordVehicles));
+            var resultList = fordVehicles.VehiclesToDtoList(fordVehicles, _mapper);
+
+            return new JsonResult(resultList);
         }
         catch (Exception)
         {
@@ -143,12 +156,13 @@ public class VehicleController : ControllerBase
             return BadRequest(new ResultViewModel<RegisterVehicleViewModel>(ModelState.GetErrors()));
         try
         {
-            var vehicle = new Vehicle();
-            var createdVehicle = vehicle.Create(viewModel);
+            var createdVehicle = new Vehicle().Create(viewModel);
+            var vehicleDto = _mapper.Map<VehicleDTO>(createdVehicle).Display();
+
             await _ctx.Vehicles.AddAsync(createdVehicle);
             await _ctx.SaveChangesAsync();
 
-            return Created($"vehicles/v1/{createdVehicle.LicensePlate}", new ResultViewModel<Vehicle>(createdVehicle));
+            return Created($"vehicles/v1/{createdVehicle.LicensePlate}", new JsonResult(vehicleDto));
         }
         catch (DbUpdateException)
         {
@@ -183,9 +197,6 @@ public class VehicleController : ControllerBase
         [FromBody] UpdateVehicleViewModel viewModel,
         [FromQuery(Name = apiKeyName)] string apiKeyName)
     {
-        if (viewModel.CheckIfAllEmpty(viewModel))
-            return BadRequest(new ResultViewModel<UpdateVehicleViewModel>("Cannot update infos if all values are empty"));
-
         if (!ModelState.IsValid)
             return BadRequest(new ResultViewModel<Vehicle>(ModelState.GetErrors()));
         try
@@ -195,10 +206,12 @@ public class VehicleController : ControllerBase
                 return NotFound(new ResultViewModel<Vehicle>("01EX3000 - Vehicle not found."));
 
             vehicle.Update(viewModel);
+            var updatedDto = _mapper.Map<VehicleDTO>(vehicle).Display();
+
             _ctx.Update(vehicle);
             await _ctx.SaveChangesAsync();
 
-            return Ok(new ResultViewModel<Vehicle>(vehicle));
+            return new JsonResult(updatedDto);
         }
         catch
         {
@@ -230,14 +243,85 @@ public class VehicleController : ControllerBase
             if (vehicle == null)
                 return NotFound(new ResultViewModel<Vehicle>("01EX4000 - Vehicle not found."));
 
+            var deletedDto = _mapper.Map<VehicleDTO>(vehicle).Display();
+            
             _ctx.Vehicles.Remove(vehicle);
             await _ctx.SaveChangesAsync();
 
-            return Ok(new ResultViewModel<Vehicle>(vehicle));
+            return Ok(new JsonResult(deletedDto));
         }
         catch (Exception)
         {
             return StatusCode(500, new { message = "01EX4001 - Internal server error" });
         }
+    }
+
+    /// <summary>
+    /// Registers a vehicle departure
+    /// </summary>
+    /// <param name="licensePlate">vehicle license plate</param>
+    /// <param name="apiKeyName">API key</param>
+    /// <returns>vehicle data</returns>
+    /// <response code="200">Success</response>
+    /// <response code="401">Unauthorized</response>
+    /// <response code="404">Not Found</response>
+    /// <response code="500">Internal Server Error</response>
+    [HttpPut("v1/vehicles/departure/{licensePlate}")]
+    [ApiKey]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public virtual async Task<IActionResult> DepartureAsync(
+        [FromRoute] string licensePlate,
+        [FromQuery(Name = apiKeyName)] string apiKeyName)
+    {
+        try
+        {
+            var vehicle = await _ctx.Vehicles.FirstOrDefaultAsync(x => x.LicensePlate == licensePlate);
+            if (vehicle == null)
+                return NotFound(new ResultViewModel<Vehicle>("01EX4001 - Vehicle not found."));
+
+            vehicle.Departure();
+            _ctx.Update(vehicle);
+            await _ctx.SaveChangesAsync();
+
+            return Ok(new JsonResult(new { message = "Vehicle has exited" }));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new { message = "01EX4003 - Internal server error" });
+        }
+    }
+
+    /// <summary>
+    /// Reactivate a vehicle which already has been in the parking lot
+    /// </summary>
+    /// <param name="licensePlate">vehicle license plate</param>
+    /// <param name="apiKeyName">API key</param>
+    /// <returns>reactivation confirmation</returns>
+    /// <response code="200">Success</response>
+    /// <response code="401">Unauthorized</response>
+    /// <response code="404">Not Found</response>
+    /// <response code="500">Internal Server Error</response>
+    [HttpPut("v1/vehicles/reentered/{licensePlate}")]
+    [ApiKey]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public virtual async Task<IActionResult> ReenteredAsync(
+        [FromRoute] string licensePlate,
+        [FromQuery(Name = apiKeyName)] string apiKeyName)
+    {
+        var vehicle = _ctx.Vehicles.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        if (vehicle == null)
+            return NotFound(new ResultViewModel<Vehicle>("01EX4004 - Vehicle not found."));
+
+        vehicle.Reentered();
+        _ctx.Update(vehicle);
+        await _ctx.SaveChangesAsync();
+
+        return new JsonResult(new { isActive = vehicle.IsActive, lastUpdate = vehicle.LastUpdateDate });
     }
 }
