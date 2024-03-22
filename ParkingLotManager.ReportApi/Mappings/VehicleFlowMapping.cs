a5ff48c4753bc3ba46491b49beb317aa5995972a
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-22 15:36:08 -0300

    add ReportAPI.Mappings

---
+++ ParkingLotManager.ReportApi/Mappings/VehicleFlowMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ParkingLotManager.ReportApi.DTOs;
+using ParkingLotManager.ReportApi.Models;
+
+namespace ParkingLotManager.ReportApi.Mappings;
+
+public class VehicleFlowMapping : Profile
+{
+    public VehicleFlowMapping()
+    {
+        CreateMap<VehicleModel, VehicleModelDto>();
+
+        CreateMap(typeof(ParkingLotGenericResponseDto<>), typeof(ParkingLotGenericResponseDto<>));        
+    }
+}
