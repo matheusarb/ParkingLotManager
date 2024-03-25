f139b60bd3de992500965d61c732577e94935086
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-03-25 13:53:24 -0300

    override SaveChangesAsync

--- ParkingLotManager.WebApi/Data/AppDataContext.cs
+++ ParkingLotManager.WebApi/Data/AppDataContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ParkingLotManager.WebApi.Data.Mappings;
+using ParkingLotManager.WebApi.Enums;
 using ParkingLotManager.WebApi.Models;
 using System.Diagnostics;
 
@@ -8,12 +9,12 @@ namespace ParkingLotManager.WebApi.Data;
 public class AppDataContext : DbContext
 {
     protected AppDataContext()
-    {        
+    {
     }
 
     public AppDataContext(DbContextOptions options)
         : base(options)
-    {        
+    {
     }
 
     public virtual DbSet<Company> Companies { get; set; }
@@ -25,5 +26,32 @@ public class AppDataContext : DbContext
         modelBuilder.ApplyConfiguration(new CompanyMap());
         modelBuilder.ApplyConfiguration(new VehicleMap());
         modelBuilder.ApplyConfiguration(new UserMap());
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var pendingChanges = ChangeTracker.Entries<Vehicle>()
+            .Where(e => e.State == EntityState.Added);
+
+        foreach (var entry in pendingChanges)
+        {
+            var vehicle = entry.Entity;
+            var company = Companies.FirstOrDefault(c => c.Name == vehicle.CompanyName);
+
+            if (company != null)
+            {
+                if (vehicle.Type == EVehicleType.Car)
+                {
+                    company.CarSlots--;
+                }
+                else if (vehicle.Type == EVehicleType.Motorcycle)
+                {
+                    company.MotorcycleSlots--;
+                }
+            }
+        }
+
+        // Salve as alterações no banco de dados
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
